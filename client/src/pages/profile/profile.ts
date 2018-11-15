9b75583b9e6925afada71f35606b8f76ada65289
Valeria <valeriasalasp1997@gmail.com>
2018-11-14 20:07:22 -0400

    User profile edit

--- client/src/pages/profile/profile.ts
+++ client/src/pages/profile/profile.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MyProductsPage } from '../my-products/my-products';
 import { AlertController } from 'ionic-angular';
+import { PhotoLibrary } from '@ionic-native/photo-library';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 @IonicPage()
@@ -11,9 +13,9 @@ import { AlertController } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private photoLibrary: PhotoLibrary, private camera: Camera) {
   }
-  myProducts(){
+  myProducts() {
     this.navCtrl.push(MyProductsPage);
   }
 
@@ -43,6 +45,43 @@ export class ProfilePage {
       ]
     });
     prompt.present();
+  }
+
+  deleteUser() {
+    const confirm = this.alertCtrl.create({
+      title: 'Are you sure you want to delete this account?',
+      message: 'This will permanently delete your account',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  changePicture() {
+    console.log('camera');
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    });
   }
 
   ionViewDidLoad() {
