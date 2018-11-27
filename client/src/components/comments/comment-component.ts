0e4ef922948244380a0692c9628837e74818533a
Valeria <valeriasalasp1997@gmail.com>
2018-11-27 02:57:30 -0400

    Function delete comment alert

--- client/src/components/comments/comment-component.ts
+++ client/src/components/comments/comment-component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NavController, ViewController, ModalController } from 'ionic-angular';
+import { NavController, ViewController, ModalController, AlertController } from 'ionic-angular';
 import { ViewProductPage } from '../../pages/view-product/view-product';
 import { CommentProvider } from '../../providers/comment/comment';
 import { HomePage } from '../../pages/home/home';
@@ -10,20 +10,36 @@ import { HomePage } from '../../pages/home/home';
   templateUrl: 'comment-component.html'
 })
 export class CommentComponent {
-  @Input() comments:any = {};
+  @Input() comments: any = {};
   text: string;
   delete: boolean = true;
 
-  constructor(public navCtrl: NavController, private viewCtrl: ViewController,  public commentProvider: CommentProvider, public modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private viewCtrl: ViewController, public commentProvider: CommentProvider, public modalCtrl: ModalController) {
     console.log('Hello CommentsComponent Component');
     this.text = 'Hello World';
     // console.log(this.comments);
   }
 
-  openModal() {
-
-    let modal = this.modalCtrl.create(HomePage);
-    modal.present();
+  deleteComment() {
+    const confirm = this.alertCtrl.create({
+      title: 'Do you want to delete this comment?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Yes');
+          }
+        },
+        {
+          text: 'No',
+          handler: () => {
+            console.log('No');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
-
 }
+
+
