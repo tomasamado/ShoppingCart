d6184e7d73d8a224c7d868c9c880bffb62ce3f5a
Tom <tomasamadopeters@gmail.com>
2018-11-29 21:35:37 -0400

    Add to cart

--- client/src/pages/view-product/view-product.ts
+++ client/src/pages/view-product/view-product.ts
@@ -5,6 +5,8 @@ import { HomePage } from '../home/home';
 import { CommentsPage } from '../comments/comments';
 import { UserProvider } from '../../providers/user/user'
 import { CartProvider } from '../../providers/cart/cart';
+import { ProductComponent } from '../../components/product-component/product-component';
+import { ProductProvider } from '../../providers/product/product';
 
 
 @IonicPage()
@@ -15,11 +17,13 @@ import { CartProvider } from '../../providers/cart/cart';
 export class ViewProductPage {
   product: any;
   comments: any = {};
-  user:any;
-  cars:any=['BMW','Ferari','Audi','Bugati', 'a', 'aaaaa', 'a', 'a','a','a', 'a','a'];
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController , public navParams: NavParams, public modalCtrl: ModalController, public userProvider: UserProvider , public toastCtrl: ToastController, public commentProvider: CommentProvider) {
-  cartProduct:any ={};
   user: any;
+  cars: any = ['BMW', 'Ferari', 'Audi', 'Bugati', 'a', 'aaaaa', 'a', 'a', 'a', 'a', 'a', 'a'];
+  cartProduct: any = {};
+  cart: any = {};
+  existsInCart: boolean;
+  vCart: any = {};
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,public productProvider: ProductProvider, public navParams: NavParams, public modalCtrl: ModalController, public userProvider: UserProvider, public toastCtrl: ToastController, public commentProvider: CommentProvider, public cartProvider: CartProvider) {
     this.product = navParams.get('product');
     this.user = navParams.get('user');
 
@@ -27,7 +31,10 @@ export class ViewProductPage {
 
   ionViewDidEnter() {
     this.getComments(this.product.id);
-    this.cartProduct = this.product;
+    console.log(this.product.quantity);
+    console.log("product initialy is "+ this.product.id)
+    this.cartProduct = JSON.parse(JSON.stringify(this.product));
+
   }
 
 
@@ -53,14 +60,65 @@ export class ViewProductPage {
         {
           text: 'Add',
           handler: data => {
-            this.cartProduct.user_id = this.user.id;
-            this.addToCart(this.cartProduct);
+            this.cartProduct
+            this.existsInCart = false;
+            if (data.quantity<=this.product.quantity){
+            this.cartProvider.getCart().subscribe(datas => {
+                this.vCart = datas
+                this.cartProduct.product_id = this.product.id;
+                this.vCart.forEach(element => {
+                  if (element.product_id == this.product.id){
+                  this.existsInCart = true;
+                  this.cartProduct.id = element.id;
+                  console.log(element)
+                  console.log("quintiturs before the sumers " + this.cartProduct.quantity)
+                  this.cartProduct.quantity = (+element.quantity + +data.quantity);
+                  console.log("ye biu")
+                  }
+                });
+                console.log(this.existsInCart);
+
+                console.log("the carts produict cuqntity is now " + this.cartProduct.quantity)
+
+                this.cartProduct.user_id = this.user.id;
+
+                this.product.quantity = (this.product.quantity - data.quantity);
+
+                console.log("this produs quaintut is now " + this.product.quantity);
+                console.log("THIS PR" + this.product.id)
+
+                if (this.existsInCart){
+
+                  this.cartProvider.updateCart(this.cartProduct).subscribe((result) => {
+                    this.productProvider.updateProduct(this.product).subscribe((result) => {
+                      console.log(result);
+                    });
+                    console.log(result);
+                  });
+                }else{
+                  this.cartProduct.quantity = data.quantity;
+                  this.cartProvider.createCart(this.cartProduct).subscribe((result) => {
+                    this.productProvider.updateProduct(this.product).subscribe((result) => {});
+
+                    console.log(result);
+                  });
+                } 
+            });
             console.log('Added');
             const toast = this.toastCtrl.create({
               message: 'Product added to cart',
               duration: 1600
             });
             toast.present();
+            }else{
+              let alert = this.alertCtrl.create({
+                title: 'You numbnut',
+                subTitle: 'There arent that many products available',
+                buttons: ['Dismiss']
+              });
+        
+              alert.present();
+            }
           }
         }
       ]
@@ -71,15 +129,6 @@ export class ViewProductPage {
 
   viewComments() {
     this.navCtrl.push(CommentsPage, { comments: this.comments, product: this.product, user: this.user });
-  }
-
-  addToCart(product) {
-    this.cartProvider.createCart(product).subscribe((result) => {
-      console.log(result);
-    }, (err) => {
-      console.log(err);
-    });
-    this.navCtrl.pop();
   }
 
   getComments(id) {
